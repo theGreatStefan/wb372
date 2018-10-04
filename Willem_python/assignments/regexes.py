c77a51684d65c4da6bffe84095fc5fc99a0d00f0
Stefan <20273401@sun.ac.za>
2018-10-04 18:28:09 +0200

    Assignment almost done

--- Willem_python/assignments/regexes.py
+++ Willem_python/assignments/regexes.py
@@ -12,7 +12,48 @@ def is_integer(str):
 	else:
 		return False
 
-def is_english_number:
+def is_english_number(str):
 	'''Questio 2'''
-	pattern = re.compile('\d{1,3}|\d{1,3},\d{1,3}|\d{1,3},\d{1,3},\d{1,3}')
-	pass
+	pattern = re.compile('^\d{1,3}(,\d{3})*$')
+	pattern1 = re.compile('^\d{1,3}$')
+	pattern2 = re.compile('^\d{1,3},\d{3}$')
+	pattern3 = re.compile('^\d{1,3},\d{3},\d{3}$')
+	if pattern.match(str):
+		return True
+	else:
+		return False
+	
+def is_version(str):
+	'''Question 3'''
+	pattern = re.compile('^v( )*\d{1,3}$|^version( )*\d{1,3}$')
+	if pattern.match(str):
+		return True
+	else:
+		return False
+	
+def is_uuid(str):
+	'''Question 4'''
+	pattern = re.compile('^([a,b,c,d,e,f,\d]){8}(-([a,b,c,d,e,f,\d]){4}){3}-([a,b,c,d,e,f,\d]){12}$')
+	if pattern.match(str):
+		return True
+	else:
+		return False
+
+def is_ipv4_addr(str):
+	'''Question 5'''
+	pattern = re.compile('^((25[0-5]|2[0-4][0-9]|[1]?[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|[1]?[1-9]?[0-9])$')
+	if pattern.match(str):
+		return True
+	else:
+		return False
+
+def is_email_addr(str):
+	'''Question 6'''
+	pattern = re.compile('^[a-z,A-Z]([\w,\d,\.,!,#,$,%,&,\',*,+,\-,\/,=,?,^,`,{,|,},~])+\w@([\w])[\w,-]*([\w])+((\.([\w])[\w,-]*([\w]))+)$')
+	if pattern.match(str):
+		return True
+	else:
+		return False
+		
+
+		
